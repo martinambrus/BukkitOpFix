225e5976f52348f7db8f7124d00b892cd07d6477
Martin Ambrus <martin@martinambrus.com>
2016-03-06 18:44:57 +0100

    added support for ops.json file for per-user OP level checking

--- src/com/martinambrus/bukkitopfix/BukkitOpFix.java
+++ src/com/martinambrus/bukkitopfix/BukkitOpFix.java
@@ -24,8 +24,14 @@ package com.martinambrus.bukkitopfix;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 import org.bukkit.Bukkit;
@@ -37,14 +43,17 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.server.ServerCommandEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import com.google.gson.Gson;
 
 import net.md_5.bungee.api.ChatColor;
 
 public class BukkitOpFix extends JavaPlugin implements Listener {
 
 	private Integer opLevel = 0;
-	private List<String> disabledOpCommands = new ArrayList<String>();
+	private Map<String, Integer> perPlayerLevel = new HashMap<String, Integer>();
 
 	@Override
 	public void onEnable() {
@@ -52,10 +61,12 @@ public class BukkitOpFix extends JavaPlugin implements Listener {
 		getConfig().options().copyDefaults(true);
 		saveConfig();
 
-		// get current OP permission level setting
+		// get current global OP permission level setting
 		File f = new File("server.properties");
 		opLevel = Integer.parseInt(getString("op-permission-level", f));
-		disabledOpCommands = this.getConfig().getStringList("level" + opLevel + "disabledCommands");
+
+		// get per-player OP levels
+		reloadOpsCache();
 
 		this.getServer().getPluginManager().registerEvents(this, this);
 
@@ -63,9 +74,34 @@ public class BukkitOpFix extends JavaPlugin implements Listener {
         	Metrics metrics = new Metrics(this);
             metrics.start();
 		} catch (IOException e) {
-			Bukkit.getLogger().warning("Failed to initialize Metrics.");
+			Bukkit.getLogger().warning("[BukkitOpFix] Failed to initialize Metrics.");
 		}
     }
+
+	@SuppressWarnings("unchecked")
+	private void reloadOpsCache() {
+		String opsFileContent;
+		perPlayerLevel = new HashMap<String, Integer>();
+		try {
+			opsFileContent = readFile("ops.json", StandardCharsets.UTF_8);
+			List<Object> ops = new Gson().fromJson(opsFileContent, ArrayList.class);
+
+			for (Object pair : ops) {
+				Map<String, Object> values = (Map<String, Object>) pair;
+
+				perPlayerLevel.put((String) values.get("uuid"), ((Double) values.get("level")).intValue());
+			}
+		} catch (IOException e1) {
+			Bukkit.getLogger().warning("[BukkitOpFix] Failed to load data from ops.json file.");
+			e1.printStackTrace();
+		}
+	}
+
+	private String readFile(String path, Charset encoding) throws IOException
+	{
+	  byte[] encoded = Files.readAllBytes(Paths.get(path));
+	  return new String(encoded, encoding);
+	}
 
     public static String getString(String s, File f)
     {
@@ -88,6 +124,11 @@ public class BukkitOpFix extends JavaPlugin implements Listener {
     public void onCommand(PlayerCommandPreprocessEvent event) {
     	Player player = event.getPlayer();
 
+    	// check if we're running OP or DEOP operation and update the ops cache in such case
+    	if (event.getMessage().startsWith("/op") || event.getMessage().startsWith("/deop")) {
+    		reloadOpsCache();
+    	}
+
     	if (player.isOp()) {
     		// check the command against currently disabled vanilla OP commands
     		String cmd = event.getMessage().replace("/", "");
@@ -95,6 +136,10 @@ public class BukkitOpFix extends JavaPlugin implements Listener {
     		if (cmd.contains(" ")) {
     			cmd = cmd.split(" ")[0];
     		}
+
+    		List<String> disabledOpCommands = new ArrayList<String>();
+    		Integer localOpLevel = (perPlayerLevel.containsKey(player.getUniqueId().toString()) ? perPlayerLevel.get(player.getUniqueId().toString()) : opLevel);
+    		disabledOpCommands = this.getConfig().getStringList("level" + localOpLevel + "disabledCommands");
 
     		if (disabledOpCommands.contains(cmd.toLowerCase())) {
     			player.sendMessage(ChatColor.RED + this.getConfig().getString("disallowMessage"));
@@ -104,9 +149,19 @@ public class BukkitOpFix extends JavaPlugin implements Listener {
     }
 
     @EventHandler(priority=EventPriority.LOWEST, ignoreCancelled=true)
+	public void redirectConsoleCommand(ServerCommandEvent event) {
+    	// check if we're running OP or DEOP operation and update the ops cache in such case
+    	if (event.getCommand().startsWith("op") || event.getCommand().startsWith("deop")) {
+    		reloadOpsCache();
+    	}
+    }
+
+    @EventHandler(priority=EventPriority.LOWEST, ignoreCancelled=true)
     public void onPlayerInteract(PlayerInteractEvent event) {
-        if (event.getAction().equals(Action.RIGHT_CLICK_BLOCK) && event.getClickedBlock().getType().equals(Material.COMMAND)) {
-        	if (opLevel < 2) {
+    	// check this player's OP level
+    	Integer localOpLevel = (perPlayerLevel.containsKey(event.getPlayer().getUniqueId().toString()) ? perPlayerLevel.get(event.getPlayer().getUniqueId().toString()) : opLevel);
+    	if (event.getAction().equals(Action.RIGHT_CLICK_BLOCK) && event.getClickedBlock().getType().equals(Material.COMMAND)) {
+        	if (localOpLevel < 2) {
         		event.setCancelled(true);
         		event.getPlayer().sendMessage(ChatColor.RED + this.getConfig().getString("disallowMessage"));
         		event.getPlayer().closeInventory();
